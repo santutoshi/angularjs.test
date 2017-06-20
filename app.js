d073e858f92baca56da5ae9f37d050b1ee7bc73d
zantu <santutoshi@hotmail.com>
2017-06-20 13:16:15 +0545

    next versio web file.

---
+++ app.js
@@ -0,0 +1,26 @@
+var student ={
+	name: "", 
+	type: "student"
+};
+document.addEventListener('DOMContentLoaded', contentLoaded);
+
+function contentLoaded(event){
+	document.getElementById('name').addEventListener("keyup", keyUp);
+}
+
+function keyUp(event) {
+	// body...
+	calculateNumericOutput();
+}
+
+function calculateNumericOutput(){
+	student.name =document.getElementById('name').value;
+	var totalNameValue =0; 
+	for (var i = 0; i < student.name.length; i++) {
+		totalNameValue +=student.name.charCodeat(i);
+	}
+
+	//Insert result into page
+	var output ="total Numeric value of the person's name is " + totalNameValue;
+	document.getElementById('output').innerText =output;
+}
